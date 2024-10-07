88a0f6cc21494df28bcf33ca3e5572f04ff2af98
Rodin Opuz <opuzrodin@gmail.com>
2024-10-07 14:45:09 +0100

    Update exercise-5-elementary subtraction.py

--- Foundation Year/Lab Work/Week 2/exercise-5-elementary subtraction.py
+++ Foundation Year/Lab Work/Week 2/exercise-5-elementary subtraction.py
@@ -1,10 +1,3 @@
 one = input("Enter a number between 100 - 499: ")
 two = input("Enter a number between 100 - 499: ")
-if int(one) < int(two):
-    print("Error message. ")
-else:
-    if one[2] < two[2]:
-        
-        one[2] = one[2] + 10 - one[]
-    else:
-        one[2]
+if 100 <= int(one) <= 499 and 100 <= int(two) <= 499 and one > two:
