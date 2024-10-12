3b0a31d32200791455898200afb12874fc79cdf0
Rodin Opuz <opuzrodin@gmail.com>
2024-10-12 13:06:27 +0100

    Add files via upload

---
+++ Foundation Year/Prereading Work/introduce-app.py
@@ -0,0 +1,7 @@
+print('Welcome back to school! Answer these 3 questions to introduce yourself!')
+name = input("What is your name? ")
+colour = input("What is your favourite colour? ")
+food = input("What is your favourite food? ")
+show = input("What is your favourite TV show? ")
+
+print(f"Everyone, meet {name}! {name}'s favorite color is {color}. {name}'s favorite food is {food}. {name}'s favorite TV show is {show}.")
