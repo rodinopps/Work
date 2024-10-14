1781c79a82aa4767c396294a6dbb5e06b0e5278c
Rodin Opuz <opuzrodin@gmail.com>
2024-10-14 14:03:49 +0100

    Update exercise-5-elementary subtraction.py

--- Foundation Year/Lab Work/Week 2/exercise-5-elementary subtraction.py
+++ Foundation Year/Lab Work/Week 2/exercise-5-elementary subtraction.py
@@ -2,24 +2,24 @@ first_number = input("Enter first number:\n")
 second_number = input("Enter second number:\n")
 first_digit, second_digit, third_digit, borrow = 0, 0, 0, 0
 if first_number < second_number:
- print("First number should be greater")
+    print("First number should be greater")
 else:
- if int(first_number[2])<int(second_number[2]):
- third_digit = 10 + int(first_number[2]) - int(second_number[2])
- borrow = 1
- else:
- third_digit = int(first_number[2])-int(second_number[2])
- if int(first_number[1])<int(second_number[1]):
- second_digit = 10 + int(first_number[1]) - int(second_number[1])-borrow
- borrow = 1
- else:
- second_digit = int(first_number[1])-int(second_number[1]) - borrow
- borrow = 0
- first_digit = (int(first_number[0])-int(second_number[0])-borrow)
- if first_digit == 0:
- if second_digit == 0:
- print(f"The result is: {third_digit}")
- else:
- print(f"The result is: {second_digit}{third_digit}")
- else:
- print(f"The result is: {first_digit}{second_digit}{third_digit}")
+    if int(first_number[2])<int(second_number[2]):
+        third_digit = 10 + int(first_number[2]) - int(second_number[2])
+        borrow = 1
+    else:
+        third_digit = int(first_number[2])-int(second_number[2])
+    if int(first_number[1])<int(second_number[1]):
+        second_digit = 10 + int(first_number[1]) - int(second_number[1])-borrow
+        borrow = 1
+    else:
+        second_digit = int(first_number[1])-int(second_number[1]) - borrow
+        borrow = 0
+    first_digit = (int(first_number[0])-int(second_number[0])-borrow)
+    if first_digit == 0:
+        if second_digit == 0:
+            print(f"The result is: {third_digit}")
+        else:
+            print(f"The result is: {second_digit}{third_digit}")
+    else:
+        print(f"The result is: {first_digit}{second_digit}{third_digit}")
