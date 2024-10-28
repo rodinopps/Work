e4c12a3aa09a4900fcf12cf632fdd7d19124fbdf
Rodin Opuz <opuzrodin@gmail.com>
2024-10-28 14:45:33 +0000

    Update exercise-4-numbers.py

--- Foundation Year/Lab Work/Week 5/exercise-4-numbers.py
+++ Foundation Year/Lab Work/Week 5/exercise-4-numbers.py
@@ -1,4 +1,4 @@
-number = {
+'''number = {
     1: "one", 2: "two", 3: "three", 4: "four", 5: "five",
     6: "six", 7: "seven", 8: "eight", 9: "nine", 10: "ten",
     11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen",
@@ -32,4 +32,17 @@ number = {
 
 choice = int(input("Enter a number between 1 and 99: "))
 if 1 <= choice <= 99: print(number[choice])
-else: print("WRONG")
+else: print("WRONG")'''
+
+my_dict = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18:
+'eighteen', 19: 'nineteen', 20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80:
+'eighty', 90: 'ninety'}
+
+num = int(input("Enter a number between 1 and 99: "))
+if num <= 10 or num % 10 == 0:
+    print(my_dict[num])
+else:
+    key1 = (num // 10) * 10
+    key2 = num - key1
+    print(my_dict[key1], my_dict[key2])
