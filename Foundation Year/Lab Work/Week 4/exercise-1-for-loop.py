3ed15ef984185e4c6b8ae1eecdd9dcc30a11302f
Rodin Opuz <opuzrodin@gmail.com>
2024-11-04 13:28:24 +0000

    Update and rename p to exercise-1-for-loop.py

---
+++ Foundation Year/Lab Work/Week 4/exercise-1-for-loop.py
@@ -0,0 +1,2 @@
+#while loops are not always suitable. for loops are sometimes more useful for iteration and going through values in a storage.
+#for some of the exercises it doe not work and instead gives an error.
